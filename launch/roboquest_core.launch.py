c9e3c6277b4bc6c5c4fb864db4c3ffa0fdf701c6
Bill Mania <bill@manialabs.us>
2023-08-07 17:18:17 -0700

    Modified launch file to start the ArduCam node instead of the usb_cam node.

--- launch/roboquest_core.launch.py
+++ launch/roboquest_core.launch.py
@@ -24,14 +24,28 @@ def generate_launch_description():
         respawn_delay=5
     )
 
-    rq_camera_node = Node(
+    #
+    # For the ArduCam/RasPiCam
+    #
+    rq_camera_node = Node(                                                                          
         name='rq_camera_node',
-        package="usb_cam",
-        executable="usb_cam_node_exe",
-        parameters=[camera_params],
+        package="camera_ros",
+        executable="camera_node",
         respawn=True,
         respawn_delay=5
     )
+
+    #
+    # For a generic USB webcam
+    #
+    #rq_camera_node = Node(
+    #    name='rq_camera_node',
+    #    package="usb_cam",
+    #    executable="usb_cam_node_exe",
+    #    parameters=[camera_params],
+    #    respawn=True,
+    #    respawn_delay=5
+    #)
 
     return LaunchDescription([rq_base_node,
                               rq_camera_node
