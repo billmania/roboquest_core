fdc378539f9502e606dc387bc7a6bb7f213c45af
Bill Mania <bill@manialabs.us>
2023-09-27 15:00:27 -0700

    Updated rq_manage.py to VERSION 4.
    https://github.com/billmania/roboquest_core/issues/45

--- roboquest_core/rq_manage.py
+++ roboquest_core/rq_manage.py
@@ -18,7 +18,7 @@ from rq_msgs.msg import Telemetry
 from rq_msgs.msg import MotorSpeed
 from rq_msgs.msg import ServoAngles
 
-VERSION = 3
+VERSION = 4
 #
 # These two SCALE values are almost completely arbitrary. They
 # should be tuned based on the top speed of the drive motors.
