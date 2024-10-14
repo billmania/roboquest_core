5a9598e993d9b1fcec232b05eda05b7c83de184a
Bill Mania <bill@manialabs.us>
2024-10-14 09:07:38 -0500

    Made updater.py comply with syntax PEPs.
    https://github.com/billmania/roboquest_core/issues/74

--- scripts/updater.py
+++ scripts/updater.py
@@ -1,18 +1,8 @@
 #!/usr/bin/env python3
 
-import os
-from sys import exit
-from pathlib import Path, PurePath
-from requests import get
-import logging
-import json
-import docker
-from time import sleep
-import RPi.GPIO as GPIO
-
-from rq_hat import RQHAT
-
 """
+Manage the RoboQuest application.
+
 A utility script to manage the updating of RoboQuest docker images.
 It must be executing on the same host as the docker containers and
 must execute as the superuser. Its curent working directory must be
@@ -27,6 +17,21 @@ Files and directories:
     - UPDATER_DIR/updater.log
     - systemd service with auto restart
 """
+
+import json
+import logging
+import os
+from pathlib import Path, PurePath
+from sys import exit
+from time import sleep
+
+import RPi.GPIO as GPIO
+
+import docker
+
+from requests import get
+
+from rq_hat import RQHAT
 
 VERSION = 15
 HAT_SERIAL = '/dev/ttyAMA1'
@@ -92,22 +97,17 @@ CONTAINERS = {
 
 
 class RQUpdate(object):
-    """
-    All of the methods to manage the update process.
-    """
+    """All of the methods to manage the update process."""
 
     def __init__(self, fifo_path):
-        """
-        Setup all of the stuff.
-        """
-
+        """Initialize the variables."""
         self._make_dirs(DIRECTORIES)
         # TODO: Preserve the last N lines of UPDATE_LOG
         logging.basicConfig(
             filename=UPDATE_LOG,
             format='%(asctime)s %(levelname)s %(message)s',
             level=logging.INFO)
-        logging.info(f"updater.py version {VERSION} started")
+        logging.info(f'updater.py version {VERSION} started')
 
         #
         # A safety flag, to ensure the HAT serial port isn't touched
@@ -115,13 +115,13 @@ class RQUpdate(object):
         #
         self._containers_running = False
 
-        self._status_messages = list()
+        self._status_messages = []
         self._status_msg(f'updater.py version {VERSION}')
 
         self._ros_domain_id = self._get_ros_domain_id()
         logging.info(f'{self._ros_domain_id}')
 
-        self._messages = list()
+        self._messages = []
         self._client = None
         self._fifo = None
 
@@ -137,10 +137,11 @@ class RQUpdate(object):
 
     def _start_log_server(self):
         """
+        start_log_server.
+
         Create a daemon process to serve the updater log file. A daemon
         process is used so it can outlive its parent.
         """
-
         from multiprocessing import Process
         from http.server import BaseHTTPRequestHandler, HTTPServer
         from ssl import SSLContext, PROTOCOL_TLSv1_2
@@ -155,7 +156,7 @@ class RQUpdate(object):
             def get_ssl_context(certfile, keyfile):
                 context = SSLContext(PROTOCOL_TLSv1_2)
                 context.load_cert_chain(certfile, keyfile)
-                context.set_ciphers("@SECLEVEL=1:ALL")
+                context.set_ciphers('@SECLEVEL=1:ALL')
                 return context
 
             class LogServer(BaseHTTPRequestHandler):
@@ -218,38 +219,34 @@ class RQUpdate(object):
 
     def _update_in_progress(self) -> bool:
         """
+        update_in_progress.
+
         Test whether an update was in progress before this script restarted.
         """
-
         if Path(UPDATE_IN_PROGRESS).exists():
             return True
 
         return False
 
     def _reset_update_in_progress(self) -> None:
-        """
-        Indicate there isn't an update in progress.
-        """
-
+        """Indicate there isn't an update in progress."""
         Path(UPDATE_IN_PROGRESS).unlink(missing_ok=True)
 
         return
 
     def _set_update_in_progress(self) -> None:
-        """
-        Indicate there is an update in progress.
-        """
-
+        """Indicate there is an update in progress."""
         Path(UPDATE_IN_PROGRESS).touch(mode=0o444)
 
         return
 
     def _setup_shutdown(self):
         """
+        setup_shutdown.
+
         Setup the callback to call when the shutdown hardware
         signal is detected.
         """
-
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(SHUTDOWN_PIN, GPIO.IN)
         GPIO.add_event_detect(
@@ -259,19 +256,17 @@ class RQUpdate(object):
         )
 
     def _setup_docker(self):
-        """
-        Setup the docker client.
-        """
-
+        """Create the docker client."""
         self._client = docker.from_env()
 
     def _setup_fifo(self):
         """
+        setup_fifo.
+
         Ensure the FIFO rendezvous point exists and configure it for
         non-blocking reads. The non-existence of the FIFO can be interpreted
         by the UI as an update in progress.
         """
-
         try:
             os.mkfifo(self._fifo_path)
 
@@ -283,10 +278,7 @@ class RQUpdate(object):
         self._fifo = os.fdopen(fifo_fd)
 
     def close_fifo(self) -> None:
-        """
-        Close and remove the FIFO.
-        """
-
+        """Close and remove the FIFO."""
         if self._fifo:
             self._fifo.close()
         Path(self._fifo_path).unlink(missing_ok=True)
@@ -294,10 +286,7 @@ class RQUpdate(object):
         self._fifo = None
 
     def read_message(self) -> str:
-        """
-        If a message is available to read from the FIFO, read and return it.
-        """
-
+        """Read a message, if available, and then return it."""
         if not self._messages:
             for message in self._fifo.readline().split(EOL):
                 if message:
@@ -311,10 +300,11 @@ class RQUpdate(object):
 
     def _update_other_files(self):
         """
+        Update with new files.
+
         Retrieve other files from the registry, which are required by
         updater.py.
         """
-
         success = True
 
         for other_file in [CERT_FILE, KEY_FILE]:
@@ -336,6 +326,8 @@ class RQUpdate(object):
 
     def _requirements_met(self) -> bool:
         """
+        Confirm requirements are met.
+
         Confirm network connectivity is available and appears stable.
         """
         result = True
@@ -344,7 +336,7 @@ class RQUpdate(object):
         if response.status_code == 200:
             self._latest_updater_version = int(response.text)
         else:
-            logging.warning("Network connectivity requirements not met")
+            logging.warning('Network connectivity requirements not met')
             result = False
         response = get(FIRMWARE_VERSION, timeout=10.0)
         if response.status_code == 200:
@@ -356,11 +348,12 @@ class RQUpdate(object):
 
     def _update_updater(self):
         """
-        If a different version of this updater script is available, retrieve
-        and install it. This provides both an upgrade and a rollback mechanism.
-        Exit and allow systemd to restart it.
-        """
+        Update this script.
 
+        If a different version of this updater script is available, retrieve
+        and install it. This provides both an upgrade and a rollback
+        mechanism.
+        """
         if VERSION == self._latest_updater_version:
             return
 
@@ -394,6 +387,8 @@ class RQUpdate(object):
 
     def _status_msg(self, msg: str) -> None:
         """
+        Show a status message.
+
         Use the HAT screen 4 status message capability to display
         msg. The HAT is opened and closed every time this method is
         called, in order to minimize any conflict with rq_core.
@@ -401,7 +396,6 @@ class RQUpdate(object):
         this method persists them and overrides an internal RQHAT
         variable.
         """
-
         if self._containers_running:
             logging.warning('_status_msg: containers running')
             return
@@ -419,10 +413,7 @@ class RQUpdate(object):
         _hat.close()
 
     def stop_containers(self):
-        """
-        Kill any running containers.
-        """
-
+        """Kill any running containers."""
         logging.info('Stopping containers')
         for container_name in CONTAINERS:
             try:
@@ -441,6 +432,8 @@ class RQUpdate(object):
 
     def _get_latest_images(self):
         """
+        Get the latest images.
+
         Docker doesn't provide a mechanism to compare the version of a local
         image to the version on a registry.
         """
@@ -456,9 +449,9 @@ class RQUpdate(object):
 
             try:
                 logging.info(
-                    f"Pulling {image_name}"
+                    f'Pulling {image_name}'
                     f" {self._latest_image_versions[image_name]['version']}"
-                    " from the registry"
+                    ' from the registry'
                 )
                 image_registry = self._client.images.pull(
                     image_name,
@@ -470,7 +463,7 @@ class RQUpdate(object):
             if not image_local and not image_registry:
                 logging.fatal(
                     f'Image {image_name}'
-                    f", does not exist on the registry"
+                    f', does not exist on the registry'
                 )
                 continue
 
@@ -494,10 +487,11 @@ class RQUpdate(object):
 
     def _update_images(self) -> None:
         """
+        Update to latest images.
+
         Update the images if a newer version exists or
         there is no local copy.
         """
-
         if self._requirements_met():
             self.close_fifo()
             self._update_updater()
@@ -510,16 +504,15 @@ class RQUpdate(object):
 
     def _process_message(self, message: str) -> None:
         """
-        Process the message received from the UI. The defined
-        actions are : UPDATE, SHUTDOWN, REBOOT.
+        Process the message received from the UI.
 
+        The defined actions are : UPDATE, SHUTDOWN, REBOOT.
         The defined message:
             {
                 action: 'UPDATE',
                 args: ''
             }
         """
-
         try:
             command = json.loads(message)
 
@@ -544,6 +537,8 @@ class RQUpdate(object):
 
     def _get_ros_domain_id(self):
         """
+        Calculate the ROS DOMAIN ID.
+
         Calculate an integer, between 1 and 101 inclusive, which is
         unique across the universe of Raspberry Pi units. Return
         the string ROS_DOMAIN_ID={integer}.
@@ -561,10 +556,11 @@ class RQUpdate(object):
 
     def _start_containers(self, to_start: list) -> None:
         """
+        Start all defined containers.
+
         Start the containers listed in to_start. If the image for any
         container isn't available locally begin the update process.
         """
-
         images_exist = True
         for container_name in to_start:
             image_name = CONTAINERS[container_name]['image_name']
@@ -597,24 +593,25 @@ class RQUpdate(object):
                     )
 
                 except docker.errors.APIError as e:
-                    logging.warning(f"Exception starting {container_name}:"
-                                    f" {e}")
+                    logging.warning(f'Exception starting {container_name}:'
+                                    f' {e}')
 
                 else:
-                    logging.info(f"Container {container_name} started")
+                    logging.info(f'Container {container_name} started')
         else:
             self._update_images()
 
     def _check_running_containers(self):
         """
+        Confirm containers are running.
+
         Verify the containers are running, starting them if necessary.
         Intended for use in several scenarios:
             1. the images are up-to-date but a container stopped
             2. new images have been installed
             3. images are missing
         """
-
-        containers_to_start = list()
+        containers_to_start = []
 
         for container_name in CONTAINERS:
             containers = self._client.containers.list(
@@ -632,18 +629,15 @@ class RQUpdate(object):
             self._start_containers(containers_to_start)
 
     def _make_dirs(self, directories=[]) -> None:
-        """
-        Ensure the listed directories exist.
-        """
-
+        """Ensure the listed directories exist."""
         for directory in directories:
             d = Path(directory)
             d.mkdir(mode=0o775, exist_ok=True)
 
     def _restore_config(self, config_file: str) -> None:
         """
-        Restore config_file with its old version, if the old version
-        exists.
+        Restore config_file with its old version.
+
         In the special case where neither the config file nor its
         old version exists, the condition will be logged but nothing
         else will be done. This case occurs once with a
@@ -651,8 +645,7 @@ class RQUpdate(object):
         failure. In either event, a restart of the robot will cause
         the rq_ui container to install a default config file.
         """
-
-        logging.warn(f"Attempting to restore {config_file}")
+        logging.warn(f'Attempting to restore {config_file}')
         config_file_path = Path(OS_PERSIST_DIR) / config_file
         old_config_file_path = Path(OS_PERSIST_DIR) / (config_file + '.old')
 
@@ -660,23 +653,24 @@ class RQUpdate(object):
             try:
                 config_file_path.unlink(missing_ok=True)
                 old_config_file_path.rename(config_file_path)
-                logging.info(f"{config_file} restored from old version")
+                logging.info(f'{config_file} restored from old version')
 
             except Exception as e:
-                logging.warn(f"Failed to restore {config_file}: {e}")
+                logging.warn(f'Failed to restore {config_file}: {e}')
         else:
-            logging.warn(f"Old version of {config_file} does not exist")
+            logging.warn(f'Old version of {config_file} does not exist')
 
         return
 
     def _get_installed_image_versions(self) -> list[tuple[str, str]]:
         """
+        Get the image versions.
+
         Query the installed images and retrieve their "version"
         LABEL. Return a list of tuples containing the name and
         version for each image.
         """
-
-        installed_images = list()
+        installed_images = []
         for container_name in CONTAINERS:
             image = self._client.images.get(
                 CONTAINERS[container_name]['image_name']
@@ -687,10 +681,11 @@ class RQUpdate(object):
 
     def _write_version_object(self) -> None:
         """
+        Write the version object to a file.
+
         Collect the versions of the installed images and other components.
         Create and write ALL_VERSIONS.
         """
-
         all_versions = {
             'installed': {
                 'updater': VERSION
@@ -719,10 +714,11 @@ class RQUpdate(object):
 
     def _publish_versions(self):
         """
+        Publish the software versions.
+
         Called once when this script starts, before any containers are
         started. It's  expected this method will usually fail, because the
         robot usually does NOT have Internet connectivity.
-
         It extracts the "version" label from the local Docker images, the
         files UPDATE_VERSION and IMAGE_VERSIONS from the registry, and
         the VERSION constant from this script. All of that information is used
@@ -730,7 +726,6 @@ class RQUpdate(object):
         When images are updated, it's expected that this script will be
         restarted after the updates complete.
         """
-
         if self._requirements_met():
             #
             # Network connectivity exists and the latest version
@@ -740,7 +735,7 @@ class RQUpdate(object):
             if response.status_code == 200:
                 self._latest_image_versions = json.loads(response.text)
             else:
-                logging.warning(f"Failed to retrieve {IMAGE_VERSIONS}")
+                logging.warning(f'Failed to retrieve {IMAGE_VERSIONS}')
 
         self._write_version_object()
 
@@ -748,6 +743,8 @@ class RQUpdate(object):
 
     def _check_configs(self, restore: bool = False) -> None:
         """
+        Check the configurations.
+
         Monitor configuration files for:
             1. missing
             2. empty
@@ -760,7 +757,6 @@ class RQUpdate(object):
         files must be JSON strings and must have the "version"
         attribute at their top level.
         """
-
         for config_file in CONFIG_FILES:
             config_file_path = Path(OS_PERSIST_DIR) / config_file
 
@@ -770,14 +766,14 @@ class RQUpdate(object):
                     if 'version' in configuration:
                         continue
                     else:
-                        logging.warning(f"{config_file}"
-                                        " missing version attribute")
+                        logging.warning(f'{config_file}'
+                                        ' missing version attribute')
                 else:
-                    logging.warning(f"{config_file}"
-                                    " is empty")
+                    logging.warning(f'{config_file}'
+                                    ' is empty')
             else:
-                logging.warning(f"{config_file}"
-                                " does not exist")
+                logging.warning(f'{config_file}'
+                                ' does not exist')
 
             if restore:
                 self._restore_config(config_file)
@@ -785,12 +781,12 @@ class RQUpdate(object):
         return
 
     def run(self) -> None:
-
         """
+        Run the main loop.
+
         The loop for monitoring the containers and watching for
         a command to update the containers.
         """
-
         self._update_other_files()
         self._start_log_server()
         self._publish_versions()
@@ -814,11 +810,12 @@ class RQUpdate(object):
 
     def _shutdown_cb(self, arg):
         """
+        Shutdown the robot.
+
         Called when the shutdown control signal has been detected and
         when the SHUTDOWN command is received.
         """
-
-        logging.warn("Shutdown triggered")
+        logging.warn('Shutdown triggered')
         self.stop_containers()
         self._status_msg('Shutdown triggered')
         os.system('systemctl halt')
@@ -832,11 +829,8 @@ class RQUpdate(object):
         logging.warn('Woke unexpectedly from sleep')
 
     def _reboot_cb(self, arg):
-        """
-        Called when the REBOOT command is received.
-        """
-
-        logging.info("Reboot triggered")
+        """Reboot the robot."""
+        logging.info('Reboot triggered')
         self.stop_containers()
         self._status_msg('Reboot triggered')
         os.system('systemctl reboot')
@@ -851,11 +845,12 @@ class RQUpdate(object):
 
     def _remove_old_images(self):
         """
+        Remove old images.
+
         Intended for use only at startup, before any containers
         have been started. Removes all old images, selected
         with the "dangling" filter because they don't have a tag.
         """
-
         old_images = self._client.images.list(
             filters={
                 'dangling': True
@@ -866,10 +861,10 @@ class RQUpdate(object):
                 self._client.images.remove(image=image.id)
 
             except Exception as e:
-                logging.warning(f"Failed to remove old {image.id}: {e}")
+                logging.warning(f'Failed to remove old {image.id}: {e}')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 
     rq_update = RQUpdate(UPDATE_FIFO)
     try:
