c9543f9b2c60747436d595b6a03cded84783eecc
Bill Mania <bill@manialabs.us>
2025-04-01 15:43:03 -0500

    Set all four cameras to 640x480 and YUYV format. Changed dstart.sh to not start as a daemon by default. Deleted rq_camera_node0.yaml because it's not used.
    https://github.com/billmania/roboquest_core/issues/106

--- scripts/dstart.sh
+++ scripts/dstart.sh
@@ -8,9 +8,11 @@ IMAGE=$1
 NAME=rq_core
 PERSIST_DIR="/usr/src/ros2ws/install/roboquest_core/share/roboquest_core/persist"
 
-printf "Starting %s on %s\n" "$IMAGE" "$(docker context show)"
+#ROS_DOMAIN_ID=72
+#docker run -it --rm --privileged --network host --ipc host --env "ROS_DOMAIN_ID=${ROS_DOMAIN_ID}" --device /dev/gpiomem:/dev/gpiomem --device /dev/i2c-1:/dev/i2c-1 --device /dev/i2c-6:/dev/i2c-6 --device /dev/ttyS0:/dev/ttyS0 -v /dev/shm:/dev/shm -v /var/run/dbus:/var/run/dbus -v /run/udev:/run/udev -v /opt/persist:${PERSIST_DIR} -v ros_logs:/root/.ros/log --name $NAME "$IMAGE"
 
-docker run -d --rm \
+printf "Starting %s on %s\n" "$IMAGE" "$(docker context show)"
+docker run -it --rm \
         --privileged \
         --network host \
         --ipc host \
