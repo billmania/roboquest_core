79069bfbba4f932e6ce65f152dc731177796a5dc
Bill Mania <bill@manialabs.us>
2025-04-11 15:32:27 -0500

    manage_files.py can install missing files and replace modified files.
    https://github.com/billmania/roboquest_core/issues/110

--- scripts/manage_files.py
+++ scripts/manage_files.py
@@ -4,48 +4,50 @@ A utility class to handle the installation and removal of files
 on the robot's base OS.
 """
 
+from grp import getgrnam
+from hashlib import md5
 from json import loads as jloads
+from os import chown
+from pathlib import Path
+from pwd import getpwnam
 from typing import Callable
 
 from requests import get
 
 
-CONFIG_FILE_URL = 'https://registry.q4excellence.com:5678/manage_files.json'
+BASE_URL = 'https://registry.q4excellence.com:5678/'
+CONFIG_FILE_URL = BASE_URL + 'manage_files.json'
+BASE_FILE_URL = BASE_URL + 'files/'
 GET_TIMEOUT_S = 5.0
 
 
 class ManageFiles(object):
     """Manage file installation and removal."""
 
-    def __init__(self, logger: Callable):
+    def __init__(self, logging: Callable):
         """Prepare to manage files."""
-        if logger:
-            self._log_debug = logger.debug
-            self._log_info = logger.info
-            self._log_warning = logger.warning
-            self._log_error = logger.error
+        if logging:
+            self._log_info = logging.info
+            self._log_warning = logging.warning
+            self._log_error = logging.error
         else:
-            self._log_debug = print
             self._log_info = print
             self._log_warning = print
             self._log_error = print
 
-    def _parse_config_file(self, raw_config_file) -> None:
+    def _parse_config_file(self, raw_config_file) -> dict:
         """Parse the contents of the config file."""
         try:
-            self._file_config = jloads(raw_config_file)
+            file_config = jloads(raw_config_file)
+
         except Exception as e:
             self._log_error(
                 f'Failed to parse {raw_config_file}'
                 f' : {e}'
             )
+            file_config = None
 
-        self._log_debug(
-            f"config file parsed installs: {self._file_config['install']}"
-        )
-        self._log_debug(
-            f"config file parsed removes: {self._file_config['remove']}"
-        )
+        return file_config
 
     def get_config_file(self) -> None:
         """Get the configuration file.
@@ -58,12 +60,7 @@ class ManageFiles(object):
                 CONFIG_FILE_URL,
                 timeout=GET_TIMEOUT_S
             )
-            if config_file_content.status_code == 200:
-                self._log_debug(
-                    'config file raw content:'
-                    f' {config_file_content.text}'
-                )
-            else:
+            if config_file_content.status_code != 200:
                 self._log_error(
                     'config file content status:'
                     f' {config_file_content.status_code}'
@@ -77,7 +74,10 @@ class ManageFiles(object):
             )
             self._config_file = None
 
-        self._parse_config_file(config_file_content.text)
+        else:
+            self._config_file = self._parse_config_file(
+                config_file_content.text
+            )
 
     def remove_files(self) -> None:
         """Remove files.
@@ -85,7 +85,78 @@ class ManageFiles(object):
         Use the file management object to get the list of files
         to remove. Remove those files.
         """
-        pass
+        for file in self._config_file['remove']:
+            continue
+
+    def _file_changed(self, full_path, md5sum) -> bool:
+        """Check the state of the file.
+
+        If it doesn't exist, return True.
+        If it exists but doesn't match the md5sum of the source
+        file, return True.
+        Otherwise return False.
+        """
+        if Path(full_path).exists():
+            try:
+                hasher = md5()
+                with open(full_path, 'rb') as f:
+                    hasher.update(f.read())
+                local_digest = hasher.hexdigest()
+            except Exception as e:
+                self._log_warning(
+                    f'Failed on hash: {e}'
+                )
+                return True
+
+            if local_digest == md5sum:
+                return False
+
+        return True
+
+    def _retrieve_file(self, source_file) -> str:
+        """Retrieve the source file contents."""
+        try:
+            config_file_content = get(
+                BASE_FILE_URL+source_file,
+                timeout=GET_TIMEOUT_S
+            )
+            if config_file_content.status_code == 200:
+                return config_file_content.text
+                self._log_error(
+                    'config file content status:'
+                    f' {config_file_content.status_code}'
+                )
+
+        except Exception as e:
+            self._log_warning(
+                f'Failed to retrieve source file {source_file}'
+                f' Exception: {e}'
+            )
+            return None
+
+    def _install_file(self, file_content, file_details) -> None:
+        """Install an individual file.
+
+        Install file_content into the full path and set
+        the owner, group, and mode.
+        """
+        full_path = Path(file_details['full_path'])
+        full_path.unlink(missing_ok=True)
+        try:
+            full_path.touch(mode=file_details['mode'])
+            with open(full_path, 'w') as f:
+                f.write(file_content)
+
+            chown(
+                full_path,
+                getpwnam(file_details['owner']).pw_uid,
+                getgrnam(file_details['group']).gr_gid
+            )
+        except Exception as e:
+            self._log_warning(
+                f"Failed to install {file_details['full_path']}"
+                f' Exception: {e}'
+            )
 
     def install_files(self) -> None:
         """Install files.
@@ -93,9 +164,31 @@ class ManageFiles(object):
         Use the file management object to get the list of files
         to be installed. Install those files.
         """
-        pass
+        for file in self._config_file['install']:
+            try:
+                if self._file_changed(
+                    file['details']['full_path'],
+                    file['md5sum']
+                ):
+                    file_content = self._retrieve_file(file['source'])
+                    if file_content:
+                        self._log_info(
+                            f"Installing: {file['details']['full_path']}"
+                        )
+                        self._install_file(
+                            file_content,
+                            file['details']
+                        )
+
+            except Exception as e:
+                self._log_warning(
+                    f"Exception installing: {file['details']['full_path']}"
+                    f' : {e}'
+                )
 
 
 if __name__ == '__main__':
     MF = ManageFiles(None)
     MF.get_config_file()
+    MF.install_files()
+    MF.remove_files()
